1bf040b31cf77435e1ed43433bed5b62a43ad969
Lazar Nikolov <hello@nikolovlazar.com>
2023-12-15 16:05:02 -0500

    fix: attempt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -37,17 +35,5 @@ export default defineNuxtConfig({
   },
   supabase: {
     redirect: false,
-  },
-  vite: {
-    build: {
-      sourcemap: true,
-    },
-    plugins: [
-      sentryVitePlugin({
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-        org: "sentry-devrel",
-        project: "buffet",
-      }),
-    ],
   },
 });
